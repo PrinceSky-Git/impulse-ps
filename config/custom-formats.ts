7bd89b6d50ad2f9cfdcaeff48719b7984d7e8f40
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-12 23:52:31 +0530

    Create custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,32 @@
+export const Formats: FormatList = [
+	{
+		section: "PokèRogue Mod",
+	},
+	{
+		name: "[Gen 9] PokeRogue",
+		mod: 'pokerogue',
+		ruleset: [
+			'Cancel Mod',
+			'HP Percentage Mod',
+			'PokeRogue Classic',
+			'PokeRogue EXP Tracker',
+		],
+		DefaultLevel: 1,
+		MaxLevel: 999999,
+		banlist: [],
+	},
+	{
+		name: "[Gen 9] PokeRogue Early",
+		mod: 'pokerogue',
+		ruleset: [
+			//'Terastal Clause',
+			'Cancel Mod',
+			'HP Percentage Mod',
+			'PokeRogue Classic',
+			'PokeRogue EXP Tracker',
+		],
+		DefaultLevel: 1,
+		MaxLevel: 999999,
+		banlist: [],
+	},
+];
