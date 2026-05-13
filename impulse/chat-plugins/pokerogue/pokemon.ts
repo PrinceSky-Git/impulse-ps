d42ae7c645cc24f4a78e44fb343f02b8fbde5f18
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-13 11:03:54 +0530

    Update pokemon.ts

--- impulse/chat-plugins/pokerogue/pokemon.ts
+++ impulse/chat-plugins/pokerogue/pokemon.ts
@@ -985,7 +985,8 @@ export function packPokemon(mon: PokemonEntry): string {
 export function packAIPokemon(set: AIPokemonSet, floor = 0): string {
 	const sp = Dex.species.get(toID(set.species));
 	const name = sp.exists ? sp.name : set.species;
-	const nickname = floor > 0 ? `FLOOR:${floor}` : name;
+	const nickname = floor > 0 ? `FLOOR:${floor} | ${name}` : name;
+	//const nickname = floor > 0 ? `FLOOR:${floor}` : name;
 	const ivStr = `${set.ivs.hp},${set.ivs.atk},${set.ivs.def},${set.ivs.spa},${set.ivs.spd},${set.ivs.spe}`;
 	const evStr = `${set.evs.hp},${set.evs.atk},${set.evs.def},${set.evs.spa},${set.evs.spd},${set.evs.spe}`;
 	const movesStr = set.moves.map(m => Dex.moves.get(m).name || m).join(',');
