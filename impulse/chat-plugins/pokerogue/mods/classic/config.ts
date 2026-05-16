621d78bcd3c7545658ec18a264db1cafae3732c0
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-16 13:31:03 +0530

    Update config.ts

--- impulse/chat-plugins/pokerogue/mods/classic/config.ts
+++ impulse/chat-plugins/pokerogue/mods/classic/config.ts
@@ -15,16 +15,22 @@ export const CLASSIC_STARTERS = [
 ];
 
 export const classicConfig: ModeConfig = {
+	// Core pacing
 	biomeRotationInterval: 10,
 	bossInterval: 10,
+	startingBiome: 'Town',
+	starterLevel: 5,
+
+	// Battle format
+	generation: 9,
+	baseFormat: '[Gen 9] PokeRogue',
+
+	// Trainer & move settings
 	hasTrainers: true,
 	randomizeMoves: false,
 	randomizeAbilities: false,
-	townEscapeFloor: 10,
-	startingBiome: 'Town',
-	starterLevel: 5,
-	generation: 9,
-	baseFormat: '[Gen 9] PokeRogue',
+
+	// Economy
 	economy: {
 		startingBP: 20,
 		bpPerWin: 5,
@@ -33,24 +39,36 @@ export const classicConfig: ModeConfig = {
 		startingKeyItems: ['Exp. All', 'Exp. All', 'Exp. All'],
 		startingInventory: { pokeball: 5, greatball: 0, ultraball: 0, masterball: 0 },
 	},
+
+	// Story routing
 	storyRouting: {
 		fixedTrainerWaves: [5, 8, 25, 35, 55, 62, 64, 66, 95, 112, 114, 115, 145, 164, 165, 182, 184, 186, 188, 190, 195, 200],
 		gymLeaderInterval: 30,
 		maxGymLeaderTier: 5,
 		firstGymLeaderWaves: [20, 30],
 	},
+
+	// Battle mechanic unlocks
 	mechanicUnlocks: {
 		terastallize: 25,
 	},
+
+	// Milestone rewards
 	milestoneRewards: [
 		{ floor: 50, interval: true, itemType: 'inventory', itemName: 'masterball', amount: 1 },
 		{ floor: 10, interval: false, itemType: 'keyItem', itemName: 'Exp. Charm', amount: 1 },
 	],
+
+	// Endgame
+	maxFloor: 200,
 	lastBiome: {
 		biome: 'End',
 		floor: '191-200',
 	},
-	maxFloor: 200,
+	victoryConfig: {
+		name: 'Champion',
+		dialog: 'You have conquered all 200 floors of the Classic run! You are a true PokéRogue Champion!',
+	},
 };
 
 export const classicData: ModeData = {
@@ -58,5 +76,5 @@ export const classicData: ModeData = {
 	transitions: ClassicTransitions,
 	trainers: ClassicTrainers,
 	starters: CLASSIC_STARTERS,
-	excludedBiomes: ['Endless'],
+	excludedBiomes: ['End'],
 };
