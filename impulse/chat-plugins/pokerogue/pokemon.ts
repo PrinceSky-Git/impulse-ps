852b7c99b695ce56b172ce1b38c2ce76c99bb2c3
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-17 17:59:50 +0530

    Update pokemon.ts

--- impulse/chat-plugins/pokerogue/pokemon.ts
+++ impulse/chat-plugins/pokerogue/pokemon.ts
@@ -533,6 +533,7 @@ export interface AIPokemonSet {
 	teraType: string;
 	moves: string[];
 	gender: string;
+	types: string[];
 }
 
 export function genAIPokemon(
@@ -613,6 +614,7 @@ export function genPokemon(
 	}
 
 	const allTypes = Dex.types.all().map(t => t.name);
+	const allTypeNames = Dex.types.all().filter(t => !t.isNonstandard).map(t => t.name);
 	const gennedMons: AIPokemonSet[] = [];
 	let depth = 0;
 
@@ -749,6 +751,10 @@ export function genPokemon(
 			break;
 		}
 
+		const speciesTypes = config?.randomizeTypes
+			? finalSpecie.types.map(() => allTypeNames[Math.floor(Math.random() * allTypeNames.length)])
+			: [...finalSpecie.types];
+
 		let ivs: any;
 		if (floor <= 10) {
 			ivs = {
@@ -779,7 +785,7 @@ export function genPokemon(
 		const item = forcedItem ?? pickRandomHeldItem(finalSpecie.name);
 		const teraType = forcedTeraType ?? (Math.floor(Math.random() * 20) === 0 ?
 			allTypes[Math.floor(Math.random() * allTypes.length)] :
-			finalSpecie.types[Math.floor(Math.random() * finalSpecie.types.length)]);
+			speciesTypes[Math.floor(Math.random() * speciesTypes.length)]);
 
 		let moves: string[] = [];
 		if (config?.randomizeMoves) {
@@ -803,6 +809,7 @@ export function genPokemon(
 			teraType,
 			moves,
 			gender: finalSpecie.gender || (Math.random() < 0.5 ? 'M' : 'F'),
+			types: speciesTypes,
 		});
 
 		depth++;
