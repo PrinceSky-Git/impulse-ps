c43210cb5bbe95c6c8160de24e963c7868c3e0b7
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-18 15:30:13 +0530

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -5,6 +5,22 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	{
 		name: "[Gen 9] PokeRogue",
 		mod: 'pokerogue',
+		searchShow: false,
+		ruleset: [
+			'Cancel Mod',
+			'HP Percentage Mod',
+			'PokeRogue Classic',
+			'PokeRogue EXP Tracker',
+		],
+		DefaultLevel: 1,
+		MaxLevel: 9999,
+		banlist: [],
+	},
+	{
+		name: "[Gen 9] PokeRogue Doubles",
+		mod: 'pokerogue',
+		searchShow: false,
+		gameType: 'doubles',
 		ruleset: [
 			'Cancel Mod',
 			'HP Percentage Mod',
