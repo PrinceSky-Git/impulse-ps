c803f2ea74f7827559f185024437a3fdb37902d4
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-26 16:40:27 +0530

    Update random-config.ts

--- impulse/chat-plugins/pokerogue/mods/random/random-config.ts
+++ impulse/chat-plugins/pokerogue/mods/random/random-config.ts
@@ -17,8 +17,8 @@ export const randomConfig: ModeConfig = {
 	baseFormat: '[Gen 9] PokeRogue',
 	doublesFormat: '[Gen 9] PokeRogue Doubles',
 	economy: {
-		startingMoney: 1000,
-		startingKeyItems: { 'Exp. All': 1},
+		startingMoney: 1500,
+		startingKeyItems: { 'Exp. All': 2},
 		startingInventory: { pokeball: 5, greatball: 0, ultraball: 0, masterball: 0 },
 	},
 	
