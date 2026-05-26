5eb2b8cd1aa7943571b0e84ed2457d299dfcf995
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-26 23:53:06 +0530

    Update shopdb.ts

--- impulse/chat-plugins/pokerogue/shopdb.ts
+++ impulse/chat-plugins/pokerogue/shopdb.ts
@@ -19,8 +19,8 @@ const getFullRestoreWeight = (state: PokeRogueState) => {
 const X_ITEMS_DATA: [string, string, string, string, ItemRarityTier][] = [
 	['xattack', "X Attack", "atk", "Raises Attack by 10% for 5 battles.", "Common"],
 	['xdefense', "X Defense", "def", "Raises Defense by 10% for 5 battles.", "Common"],
-	['xspatk', "X Sp. Atk", "spa", "Raises Sp. Atk by 10% for 5 battles.", "Common"],
-	['xspdef', "X Sp. Def", "spd", "Raises Sp. Def by 10% for 5 battles.", "Common"],
+	['xspatk', "X Special Attack", "spa", "Raises Sp. Atk by 10% for 5 battles.", "Common"],
+	['xspdef', "X Special Defense", "spd", "Raises Sp. Def by 10% for 5 battles.", "Common"],
 	['xspeed', "X Speed", "spe", "Raises Speed by 10% for 5 battles.", "Common"],
 ];
 
