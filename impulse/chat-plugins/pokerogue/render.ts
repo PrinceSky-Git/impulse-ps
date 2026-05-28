23dee4fcc4f8ca31cd9b6c2d384d3bff57e9afa0
PrinceSky-Git <princeskygithub@gmail.com>
2026-05-28 06:50:41 +0530

    Update render.ts

--- impulse/chat-plugins/pokerogue/render.ts
+++ impulse/chat-plugins/pokerogue/render.ts
@@ -342,7 +342,7 @@ function renderGachaView(user: User): string {
 		{ id: 'generic', name: 'Generic Banner', img: 'https://raw.githubusercontent.com/PrinceSky-Git/pokemon-showdown/refs/heads/master/impulse/chat-plugins/pokerogue/sprites/banners/rayquaza-banner.jpg' },
 	];
 
-	buf += `<div style="max-height: 500px; overflow-y: scroll; padding: 10px; margin-bottom: 20px;">`;
+	buf += `<div style="max-height: 1000px; overflow-y: scroll; padding: 10px; margin-bottom: 20px;">`;
 	for (const banner of banners) {
 		buf += `<div style="text-align: center; margin-bottom: 25px; padding-bottom: 15px; border-bottom: 1px solid #444;">`;
 		buf += `<img src="${banner.img}" alt="${banner.name}" style="max-width: 100%; max-height: 180px; border-radius: 6px; display: block; margin: 0 auto 12px auto; box-shadow: 0px 4px 8px rgba(0,0,0,0.3);" />`;
